9200b0f879d6e9776953f560e426ac53da96363a
aadilshajahan1102 <aadilshajahan.b21cs1102@mbcet.ac.in>
2024-07-09 23:24:01 +0530

    first

---
+++ basics.js
@@ -0,0 +1,20 @@
+let names=["Arun","Malu","Jose",123,567];
+let newnames=["Aadil","Adnan"];
+console.log(names[1]);
+console.log('The length of the list is ',names.length)
+console.log(person)
+let persons=[
+    {
+        name:"Abhishek",
+        age: 22
+    },
+    {
+        name:"Remya",
+        age:24
+    },
+    {
+        name:"Meena",
+        age:25
+    }
+]
+console.log(2.persons)
---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hey there");
+let value=6
+let a=value+5;
+{
+    let value=7;
+    console.log(value);
+}
+console.log("Result is",value);
